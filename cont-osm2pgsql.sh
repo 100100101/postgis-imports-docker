0b9c493588cb71009765d2a2eae1de0f129fc0f8
Bibik <bibik@cdu.ru>
2022-06-09 14:17:40 +0300

    ogr2ogr

---
+++ cont-osm2pgsql.sh
@@ -0,0 +1,11 @@
+PG_ENV_OSM_USER=gisuser
+POSTGRES_PASSWORD=pass
+PG_ENV_OSM_DB=gis
+PG_PORT__PORT=5432
+OSM_PBF_FILENAME=Moscow.osm.pbf
+# postgresql://[user[:password]@][netloc][:port][,...][/dbname][?param1=value1&...]
+PG_CONNECTION_URL=postgresql://$PG_ENV_OSM_USER:$POSTGRES_PASSWORD@localhost:$PG_PORT__PORT/$PG_ENV_OSM_DB
+echo "Connection url: $PG_CONNECTION_URL"
+echo "OSM filename: $OSM_PBF_FILENAME"
+
+docker run --name ubuntu_bash --rm -it osm2pgsql_ubuntu_osm2pgsql osm2pgsql --create --slim --cache 2000 --database=$PG_CONNECTION_URL ./osm2pgsql/osm/$OSM_PBF_FILENAME
---
+++ ogr2ogr/run.sh
@@ -0,0 +1,2 @@
+docker run --name gdal_container_name --rm -it ogr2ogr_gdal_image ogr2ogr -h
+# docker exec gdal_container_name ls -la
