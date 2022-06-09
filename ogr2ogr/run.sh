7a224d0b3ae90b7502fa5e581bec4e368d2c1c99
Bibik <bibik@cdu.ru>
2022-06-09 15:18:40 +0300

    ogr2ogr

--- ogr2ogr/run.sh
+++ ogr2ogr/run.sh
@@ -1,2 +1,7 @@
-docker run --name gdal_container_name --rm -it ogr2ogr_gdal_image ogr2ogr -h
+# docker run --name gdal_container_name --rm -it ogr2ogr_gdal_image ogr2ogr -h
 # docker exec gdal_container_name ls -la
+
+# RUN='docker run --name gdal_container_name --rm -it ogr2ogr_gdal_image'
+# bash -c $RUN ls -la
+# docker run --name gdal_container_name --rm -it ogr2ogr_gdal_image ogr2ogr -h
+docker run --name gdal_container_name --rm -it ogr2ogr_gdal_image ls -la
