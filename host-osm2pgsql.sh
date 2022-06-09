26f8d0aeb7beb3ee81a83f681b814c26ac23a71d
JSerGit <bibik.serej@gmail.com>
2022-06-09 10:21:26 +0300

    edits

--- host-osm2pgsql.sh
+++ host-osm2pgsql.sh
@@ -1,12 +1,13 @@
+PG_ENV_OSM_DB=gis
+POSTGRES_HOST=127.0.0.1
+PG_PORT__PORT=5432
 PG_ENV_OSM_USER=gisuser
 POSTGRES_PASSWORD=pass
-PG_ENV_OSM_DB=gis
-PG_PORT__PORT=5432
 # OSM_PBF_FILENAME=Moscow.osm.pbf
 # OSM_PBF_FILE_PATH=./osm2pgsql/osm/Moscow.osm.pbf
-OSM_PBF_FILE_PATH=./osm2pgsql/osm/Moscow.osm.pbf
+OSM_PBF_FILE_PATH=/media/s/Новый%20том/planet-220411.osm.pbf
 # postgresql://[user[:password]@][netloc][:port][,...][/dbname][?param1=value1&...]
-PG_CONNECTION_URL=postgresql://$PG_ENV_OSM_USER:$POSTGRES_PASSWORD@localhost:$PG_PORT__PORT/$PG_ENV_OSM_DB
+PG_CONNECTION_URL=postgresql://$PG_ENV_OSM_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$PG_PORT__PORT/$PG_ENV_OSM_DB
 echo "Connection url: $PG_CONNECTION_URL"
 echo "OSM file path: $OSM_PBF_FILE_PATH"
 
