889086d59a18d536a532bf101985d44205e335c0
Bibik <bibik.serej@gmail.com>
2022-06-13 13:15:01 +0000

    step

--- cont-osm2pgsql.sh
+++ cont-osm2pgsql.sh
@@ -1,11 +1,16 @@
-PG_ENV_OSM_USER=gisuser
-POSTGRES_PASSWORD=pass
-PG_ENV_OSM_DB=gis
-PG_PORT__PORT=5432
-OSM_PBF_FILENAME=Moscow.osm.pbf
-# postgresql://[user[:password]@][netloc][:port][,...][/dbname][?param1=value1&...]
-PG_CONNECTION_URL=postgresql://$PG_ENV_OSM_USER:$POSTGRES_PASSWORD@localhost:$PG_PORT__PORT/$PG_ENV_OSM_DB
-echo "Connection url: $PG_CONNECTION_URL"
-echo "OSM filename: $OSM_PBF_FILENAME"
+# !/bin/bash
+source ./.env
+source ./postgis/.env
 
-docker run --name ubuntu_bash --rm -it osm2pgsql_ubuntu_osm2pgsql osm2pgsql --create --slim --cache 2000 --database=$PG_CONNECTION_URL ./osm2pgsql/osm/$OSM_PBF_FILENAME
+# postgresql://[user[:password]@][netloc][:port][,...][/dbname][?param1=value1&...]
+PG_CONNECTION_URL=postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$INNER_POSTGRES_PORT/$POSTGRES_DB
+echo "Connection url: $PG_CONNECTION_URL"
+echo "OSM file path: $OSM_PBF_FILE_PATH"
+#
+# -c, --create - Import only
+#
+# In case the system doesn’t have much main memory, you can add the -s, --slim and --drop options.
+#
+# osm2pgsql --create --slim --cache 2000 --database=$PG_CONNECTION_URL $OSM_PBF_FILE_PATH
+
+docker run --name ubuntu_bash --rm -it osm2pgsql_ubuntu_osm2pgsql osm2pgsql --create --cache $CACHE_MB --proj=$PROJECTION_SRID --database=$PG_CONNECTION_URL $OSM_PBF_FILE_PATH
