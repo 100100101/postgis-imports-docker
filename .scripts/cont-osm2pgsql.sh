88841742e3f83293847847210ea3963752838339
JSerGit <bibik.serej@gmail.com>
2022-06-13 20:50:07 +0300

    edits

--- .scripts/cont-osm2pgsql.sh
+++ .scripts/cont-osm2pgsql.sh
@@ -1,10 +1,10 @@
 # !/bin/bash
 source ../.env
-
+RUN_NAME=run_ubuntu_osm2pgsql
 PG_CONNECTION_URL=postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$INNER_POSTGRES_PORT/$POSTGRES_DB
 echo "Connection url: $PG_CONNECTION_URL"
 echo "OSM file path: $OSM_PBF_FILE_PATH"
 echo "Cache size mb: $CACHE_MB"
 echo "Projection SRID: $PROJECTION_SRID"
 
-docker run --name ubuntu_bash --rm -it osm2pgsql_ubuntu_osm2pgsql osm2pgsql --create --cache $CACHE_MB --proj=$PROJECTION_SRID --database=$PG_CONNECTION_URL $OSM_PBF_FILE_PATH
+docker run --name $RUN_NAME --rm -it ${OSM2PGSQL_COMPOSE_PROJECT_NAME}_ubuntu_osm2pgsql osm2pgsql --create --cache $CACHE_MB --proj=$PROJECTION_SRID --database=$PG_CONNECTION_URL $OSM_PBF_FILE_PATH
--- .scripts/host-osm2pgsql.sh
+++ .scripts/host-osm2pgsql.sh
@@ -3,8 +3,11 @@ source ../.env
 
 # postgresql://[user[:password]@][netloc][:port][,...][/dbname][?param1=value1&...]
 PG_CONNECTION_URL=postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$INNER_POSTGRES_PORT/$POSTGRES_DB
+
 echo "Connection url: $PG_CONNECTION_URL"
 echo "OSM file path: $OSM_PBF_FILE_PATH"
+echo "Cache size mb: $CACHE_MB"
+echo "Projection SRID: $PROJECTION_SRID"
 #
 # -c, --create - Import only
 #
