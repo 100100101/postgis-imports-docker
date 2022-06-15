4332b750a8c2cb060c2992acc8043b98186c8dc0
JSerGit <bibik.serej@gmail.com>
2022-06-15 17:40:25 +0300

    docker compose to docker-compose

--- postgis/docker-compose-up.sh
+++ postgis/docker-compose-up.sh
@@ -3,9 +3,10 @@ DOTENV_PATH=../.env
 source $DOTENV_PATH
 # -f "./docker-compose.yml"
 COMPOSE_PROJECT_NAME=$POSTGIS_COMPOSE_PROJECT_NAME \
-    docker scompose \
+    docker-compose \
     --env-file=$DOTENV_PATH \
     up \
     -d \
     --build
+
 
