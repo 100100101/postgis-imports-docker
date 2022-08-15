b263feaa82d17b12786c46d928eb449caeb77e40
JSerGit <bibik.serej@gmail.com>
2022-08-16 01:58:39 +0300

    step

--- gdal/convert-4326to3857.sh
+++ gdal/convert-4326to3857.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 source ../.env
-SHP_FILENAME=boundary-polygon-lvl10
+# SHP_FILENAME=boundary-polygon-lvl10
 SOURCE_EPGS=4326
 TARGET_EPGS=3857
-# docker run --rm -it osgeo/gdal bash -c "ogr2ogr -f \"ESRI Shapefile\"  $CONT_SHAPES_PATH/${SHP_FILENAME}.shp $CONT_SHAPES_PATH/converted/${SHP_FILENAME}_${TARGET_EPGS}.shp -s_srs EPSG:$SOURCE_EPGS -t_srs EPSG:$TARGET_EPGS"
 
-docker run -v $HOST_SHAPES_PATH:$CONT_SHAPES_PATH --rm -it osgeo/gdal ogr2ogr -f "ESRI Shapefile" $CONT_SHAPES_PATH/converted/${SHP_FILENAME}_${TARGET_EPGS}.shp $CONT_SHAPES_PATH/${SHP_FILENAME}.shp -s_srs EPSG:$SOURCE_EPGS -t_srs EPSG:$TARGET_EPGS
+CONVERTED_DIR=$CONT_SHAPES_PATH/converted
+
+mkdir $CONVERTED_DIR
+
+docker run -v $HOST_SHAPES_PATH:$CONT_SHAPES_PATH --rm -it osgeo/gdal ogr2ogr -f "ESRI Shapefile" $CONVERTED_DIR/${SHP_FILENAME}_${TARGET_EPGS}.shp $CONT_SHAPES_PATH/${SHP_FILENAME}.shp -s_srs EPSG:$SOURCE_EPGS -t_srs EPSG:$TARGET_EPGS
 
 
 
