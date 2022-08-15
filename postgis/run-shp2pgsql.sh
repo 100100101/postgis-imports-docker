42d52b84ac9e75accdbb0a6c913bde239a866a35
Bibik <bibik@cdu.ru>
2022-08-15 19:11:15 +0300

    step

---
+++ postgis/run-shp2pgsql.sh
@@ -0,0 +1,258 @@
+#!/bin/bash
+
+# SHP_IMPORT_TABLE_NAME=building_point_3857 && \ 
+# SHP_IMPORT_FILENAME=building-point && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=aerialway_line_3857 && \ 
+# SHP_IMPORT_FILENAME=aerialway-line && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=building_polygon_3857 && \ 
+# SHP_IMPORT_FILENAME=building-polygon && \
+# ./shp2pgsql.sh
+
+
+SHP_IMPORT_TABLE_NAME=aerialway_point_3857 && \ 
+SHP_IMPORT_FILENAME=aerialway-point && \
+./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=coastlines_3857 && \ 
+# SHP_IMPORT_FILENAME=coastlines && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=airport_polygon_3857 && \ 
+# SHP_IMPORT_FILENAME=airport-polygon && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=highway_crossing_point_3857 && \ 
+# SHP_IMPORT_FILENAME=highway-crossing-point && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=highway_line_3857 && \ 
+# SHP_IMPORT_FILENAME=highway-line && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=boundary_polygon_land_lvl10_3857 && \ 
+# SHP_IMPORT_FILENAME=boundary-polygon-land-lvl10 && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=land_3857 && \ 
+# SHP_IMPORT_FILENAME=land && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=boundary_polygon_land_lvl1_3857 && \ 
+# SHP_IMPORT_FILENAME=boundary-polygon-land-lvl1 && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=landuse_polygon_3857 && \ 
+# SHP_IMPORT_FILENAME=landuse-polygon && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=boundary_polygon_land_lvl2_3857 && \ 
+# SHP_IMPORT_FILENAME=boundary-polygon-land-lvl2 && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=nature_reserve_polygon_3857 && \ 
+# SHP_IMPORT_FILENAME=nature_reserve-polygon && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=boundary_polygon_land_lvl3_3857 && \ 
+# SHP_IMPORT_FILENAME=boundary-polygon-land-lvl3 && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=parking_polygon_3857 && \ 
+# SHP_IMPORT_FILENAME=parking-polygon && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=boundary_polygon_land_lvl4_3857 && \ 
+# SHP_IMPORT_FILENAME=boundary-polygon-land-lvl4 && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=poi_point_3857 && \ 
+# SHP_IMPORT_FILENAME=poi-point && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=boundary_polygon_land_lvl5_3857 && \ 
+# SHP_IMPORT_FILENAME=boundary-polygon-land-lvl5 && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=poi_polygon_3857 && \ 
+# SHP_IMPORT_FILENAME=poi-polygon && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=boundary_polygon_land_lvl6_3857 && \ 
+# SHP_IMPORT_FILENAME=boundary-polygon-land-lvl6 && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=power_line_3857 && \ 
+# SHP_IMPORT_FILENAME=power-line && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=boundary_polygon_land_lvl7_3857 && \ 
+# SHP_IMPORT_FILENAME=boundary-polygon-land-lvl7 && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=power_point_3857 && \ 
+# SHP_IMPORT_FILENAME=power-point && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=boundary_polygon_land_lvl8_3857 && \ 
+# SHP_IMPORT_FILENAME=boundary-polygon-land-lvl8 && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=public_transport_line_3857 && \ 
+# SHP_IMPORT_FILENAME=public-transport-line && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=boundary_polygon_land_lvl9_3857 && \ 
+# SHP_IMPORT_FILENAME=boundary-polygon-land-lvl9 && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=boundary_polygon_land_3857 && \ 
+# SHP_IMPORT_FILENAME=boundary-polygon-land && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=public_transport_point_3857 && \ 
+# SHP_IMPORT_FILENAME=public-transport-point && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=boundary_polygon_lvl10_3857 && \ 
+# SHP_IMPORT_FILENAME=boundary-polygon-lvl10 && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=railway_line_3857 && \ 
+# SHP_IMPORT_FILENAME=railway-line && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=boundary_polygon_lvl1_3857 && \ 
+# SHP_IMPORT_FILENAME=boundary-polygon-lvl1 && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=railway_platform_polygon_3857 && \ 
+# SHP_IMPORT_FILENAME=railway-platform-polygon && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=boundary_polygon_lvl2_3857 && \ 
+# SHP_IMPORT_FILENAME=boundary-polygon-lvl2 && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=railway_station_point_3857 && \ 
+# SHP_IMPORT_FILENAME=railway-station-point && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=boundary_polygon_lvl3_3857 && \ 
+# SHP_IMPORT_FILENAME=boundary-polygon-lvl3 && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=settlement_point_3857 && \ 
+# SHP_IMPORT_FILENAME=settlement-point && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=settlement_polygon_3857 && \ 
+# SHP_IMPORT_FILENAME=settlement-polygon && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=boundary_polygon_lvl4_3857 && \ 
+# SHP_IMPORT_FILENAME=boundary-polygon-lvl4 && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=boundary_polygon_lvl5_3857 && \ 
+# SHP_IMPORT_FILENAME=boundary-polygon-lvl5 && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=subway_entrance_point_3857 && \ 
+# SHP_IMPORT_FILENAME=subway-entrance-point && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=boundary_polygon_lvl6_3857 && \ 
+# SHP_IMPORT_FILENAME=boundary-polygon-lvl6 && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=surface_polygon_3857 && \ 
+# SHP_IMPORT_FILENAME=surface-polygon && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=boundary_polygon_lvl7_3857 && \ 
+# SHP_IMPORT_FILENAME=boundary-polygon-lvl7 && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=vegetation_polygon_3857 && \ 
+# SHP_IMPORT_FILENAME=vegetation-polygon && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=boundary_polygon_lvl8_3857 && \ 
+# SHP_IMPORT_FILENAME=boundary-polygon-lvl8 && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=water_line_3857 && \ 
+# SHP_IMPORT_FILENAME=water-line && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=boundary_polygon_lvl9_3857 && \ 
+# SHP_IMPORT_FILENAME=boundary-polygon-lvl9 && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=water_polygon_3857 && \ 
+# SHP_IMPORT_FILENAME=water-polygon && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=boundary_polygon_3857 && \ 
+# SHP_IMPORT_FILENAME=boundary-polygon && \
+# ./shp2pgsql.sh
+
+
+# SHP_IMPORT_TABLE_NAME=water_3857 && \ 
+# SHP_IMPORT_FILENAME=water && \
+# ./shp2pgsql.sh
+
+
+
