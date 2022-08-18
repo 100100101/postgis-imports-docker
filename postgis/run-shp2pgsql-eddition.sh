792e392a42f940ba52154ac3b524c62faf9ab35e
Bibik <bibik@cdu.ru>
2022-08-18 11:30:05 +0300

    step

--- postgis/run-shp2pgsql-eddition.sh
+++ postgis/run-shp2pgsql-eddition.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-export SHP_IMPORT_TABLE_NAME=antarctica_icesheet_outlines_3857 ; export SHP_IMPORT_FILENAME=$SHP_IMPORT_TABLE_NAME ; ./shp2pgsql.sh && \
-export SHP_IMPORT_TABLE_NAME=land-polygons-split_3857 ; export SHP_IMPORT_FILENAME=$SHP_IMPORT_TABLE_NAME ; ./shp2pgsql.sh && \
-export SHP_IMPORT_TABLE_NAME=simplified-land-polygons-complete_3857 ; export SHP_IMPORT_FILENAME=$SHP_IMPORT_TABLE_NAME ; ./shp2pgsql.sh && \
-export SHP_IMPORT_TABLE_NAME=water-polygons-split_3857 ; export SHP_IMPORT_FILENAME=$SHP_IMPORT_TABLE_NAME ; ./shp2pgsql.sh
+
+export SHP_IMPORT_FILENAME=antarctica_icesheet_outlines_3857.shp ; SHP_IMPORT_TABLE_NAME=$SHP_IMPORT_FILENAME ; ./shp2pgsql.sh && \
+export SHP_IMPORT_FILENAME=antarctica_icesheet_polygons_3857.shp ; SHP_IMPORT_TABLE_NAME=$SHP_IMPORT_FILENAME ; ./shp2pgsql.sh && \
+export SHP_IMPORT_FILENAME=land_polygons_complete_3857.shp ; SHP_IMPORT_TABLE_NAME=$SHP_IMPORT_FILENAME ; ./shp2pgsql.sh && \
+export SHP_IMPORT_FILENAME=land_polygons_complete.shp ; SHP_IMPORT_TABLE_NAME=$SHP_IMPORT_FILENAME ; ./shp2pgsql.sh && \
+export SHP_IMPORT_FILENAME=land_polygons.shp ; SHP_IMPORT_TABLE_NAME=$SHP_IMPORT_FILENAME ; ./shp2pgsql.sh && \
+export SHP_IMPORT_FILENAME=land-polygons-split_3857.shp ; SHP_IMPORT_TABLE_NAME=$SHP_IMPORT_FILENAME ; ./shp2pgsql.sh && \
+export SHP_IMPORT_FILENAME=simplified-land-polygons-complete_3857.shp ; SHP_IMPORT_TABLE_NAME=$SHP_IMPORT_FILENAME ; ./shp2pgsql.sh && \
+export SHP_IMPORT_FILENAME=water-polygons-split_3857.shp ; SHP_IMPORT_TABLE_NAME=$SHP_IMPORT_FILENAME ; ./shp2pgsql.sh
